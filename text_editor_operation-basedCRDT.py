3f3919016ba81c04f44f8a0139a5a5d6f7a08e64
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2020-04-25 19:18:48 -0400

    implemented delete operation

--- text_editor_operation-basedCRDT.py
+++ text_editor_operation-basedCRDT.py
@@ -1,21 +1,9 @@
 #Implement remote_insert, query, etc. 
 #Implement simulation
 
-# Find a node with a given ID
-def find_node_with_ID(rootNode, id):
+# Return the node with the given ID
+def get_node_with_ID(rootNode, id):
     if float(rootNode.rootID) == float(id) and len(rootNode.rootID) == len(id):
-        return rootNode
-    elif float(rootNode.rootID) < float(id) or float(rootNode.rootID) == float(id):
-        #print(rootNode.rightChild.value)
-        return find_node_with_ID(rootNode.rightChild, id)
-    else: 
-        #print(rootNode.leftChild.value)
-        return find_node_with_ID(rootNode.leftChild, id)
-
-# Insert a value at the given ID
-def insert_at_ID(rootNode, id, value):
-    if float(rootNode.rootID) == float(id) and len(rootNode.rootID) == len(id):
-        rootNode.value.append(value)
         return rootNode
     else:
         #compare the next path item in the string
@@ -25,11 +13,11 @@ def insert_at_ID(rootNode, id, value):
             if rootNode.rightChild == None:
                 rootNode.setRightChild(None)
             print(rootNode.rightChild.rootID)
-            return insert_at_ID(rootNode.rightChild, id, value)
+            return get_node_with_ID(rootNode.rightChild, id)
         else: 
             if rootNode.leftChild == None:
                 rootNode.setLeftChild(None)
-            return insert_at_ID(rootNode.leftChild, id, value)
+            return get_node_with_ID(rootNode.leftChild, id)
 
 # In order traverse the tree and return the document
 def get_document(rootNode, document):
@@ -37,7 +25,10 @@ def get_document(rootNode, document):
         return None
     
     get_document(rootNode.leftChild, document)
-    document.append((rootNode.rootID, rootNode.value))
+    #Remove None values from list
+    elements = [i for i in rootNode.value if i] 
+    document.append(elements)
+    #document.append((rootNode.rootID, rootNode.value))
     get_document(rootNode.rightChild, document)
     return document
 
@@ -51,8 +42,7 @@ class client():
 
     def local_insert(self, element, previous_id):
         #get the previous node
-        previous_node = find_node_with_ID(self.rootNode, previous_id)
-        #print(previous_node.value)
+        previous_node = get_node_with_ID(self.rootNode, previous_id)
         #create new element
         safe_element = element + ":" + self.site_id
         #insert the new element as the right child
@@ -60,12 +50,25 @@ class client():
         self.element_locations.append(inserted_id)
         self.unshared_operations.append(("insert", inserted_id, safe_element))
 
-    def remote_insert(self, remote_operations):
+    def local_delete(self, element, id):
+        node = get_node_with_ID(self.rootNode, id)
+        print(node.value)
+        node.value.remove(element)
+
+    def remote_operations(self, remote_operations):
         for op in remote_operations:
-            if op[0] == "insert":
-                print(op[1])
-                #print(self.rootNode.rootID)
-                insert_at_ID(self.rootNode, op[1], op[2])
+            print(op)
+            (operation, id, val) = op
+            if operation == "insert":
+                inserted_node = get_node_with_ID(self.rootNode, id)
+                if id[len(id) - 1] == "1":
+                    inserted_node.setRightChild(val)
+                else:
+                    inserted_node.setLeftChild(val)
+                # Add id to element locations
+                self.element_locations.append(id)
+            if operation == "delete":
+                self.local_delete(val, id)
 
     @staticmethod
     def add_first_node(rootNode, value):
@@ -83,24 +86,30 @@ class uID_tree_node():
 
     def setLeftChild(self, left_child_value):
         newID = self.rootID + "0"
-        self.leftChild = uID_tree_node(newID, left_child_value)
+        if self.leftChild == None:
+            self.leftChild = uID_tree_node(newID, left_child_value)
+        else: 
+            self.leftChild.value.append(left_child_value)
         return newID
 
     def setRightChild(self, right_child_value):
         newID = self.rootID + "1"
-        self.rightChild = uID_tree_node(newID, right_child_value)
+        if self.rightChild == None:
+            self.rightChild = uID_tree_node(newID, right_child_value)
+        else: 
+            self.rightChild.value.append(right_child_value)
         return newID
 
 def main():
     # create truee
     r0 = client("site 1")
     r0.local_insert("A", "0")
-    r0.local_insert("B", "01")
+    r0.local_insert("B", "0")
 
-    print(r0.unshared_operations)
+    #print(r0.unshared_operations)
     #r0.local_insert("B", "011")
 
-    r0.remote_insert([("insert", "0", "X")])
+    r0.remote_operations([("insert", "0", "X"), ("insert", "01111", "Y"), ("delete", "01", "A:site 1")])
 
     #tree.rootNode.setRightChild("B")
     #tree.rootNode.rightChild.setLeftChild("C")
@@ -108,6 +117,12 @@ def main():
     #add_first_node(tree.rootNode, "F")
 
     #print(tree.rootNode == None)
+
+    doc = get_document(r0.rootNode, [])
+    print(doc)
+
+    #r0.local_delete("A:site 1", "01")
+    #r0.remote_insert([("delete", "01", "A:site 1")])
 
     doc = get_document(r0.rootNode, [])
     print(doc)
